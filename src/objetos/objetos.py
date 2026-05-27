62af2623c8fcda2931b38c642d6cd9305797b32f
Nyx Barros <nyxbarros6@proton.me>
2026-05-27 13:56:23 -0300

    .

--- src/objetos/objetos.py
+++ src/objetos/objetos.py
@@ -9,24 +9,20 @@ class Objetos:
             cls.gerar()
         return cls.__lista_objetos
 
-
     @classmethod
     def gerar(cls):
-        # CHIBI
-        chibi = Model(
-            "src/objetos/chibi/chibi.obj",
-            "src/objetos/chibi/chibi.png",
-            translation = [-2.0, 0.0, 0.0],
-            scale = [0.4, 0.4, 0.4]
-        )
-        cls.__lista_objetos.append(chibi)
-
-        # CAT
-        cat = Model(
-            "src/objetos/Cat/Cat.obj",
-            "src/objetos/Cat/Cat_diffuse.jpg",
-            scale=[0.12, 0.12, 0.12],
-            rotation=[90, 0, 0],
-            translation=[15.5, -1.5, 0.0]
-        )
-        cls.__lista_objetos.append(cat)
+        cls.__lista_objetos = [
+            Model( # CHIBI
+                "src/objetos/chibi/chibi.obj",
+                "src/objetos/chibi/chibi.png",
+                translation = [-2.0, 0.0, 0.0],
+                scale = [0.4, 0.4, 0.4]
+            ),
+            Model( # GATO
+                "src/objetos/Cat/Cat.obj",
+                "src/objetos/Cat/Cat_diffuse.jpg",
+                scale=[0.12, 0.12, 0.12],
+                rotation=[90, 0, 0],
+                translation=[15.5, -1.5, 0.0]
+            )
+        ]

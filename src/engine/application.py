d9aaef5fb35074d5c4045aa707a5bdf453f721dd
Nyx Barros <nyxbarros6@proton.me>
2026-05-27 12:30:40 -0300

    codigo base

--- src/engine/application.py
+++ src/engine/application.py
@@ -3,14 +3,10 @@ import glfw
 from OpenGL.GL import *
 
 import pyrr
-import numpy as np
 
-from pyrr import Vector3
+from engine.camera import Camera
 
-from src.engine.camera import Camera
-
-from src.engine.shader import Shader
-from src.engine.model import Model
+from engine.shader import Shader
 
 from objetos.objetos import Objetos
 
@@ -46,31 +42,12 @@ class Application:
 
         glViewport(0, 0, w, h)
 
-    def key_callback(
-            self,
-            window,
-            key,
-            scancode,
-            action,
-            mods
-    ):
+    def key_callback(self,window,key,scancode,action,mods):
+        if (key == glfw.KEY_ESCAPE and action == glfw.PRESS):
 
-        if (
-                key == glfw.KEY_ESCAPE
-                and action == glfw.PRESS
-        ):
+            glfw.set_window_should_close(window,True)
 
-            glfw.set_window_should_close(
-                window,
-                True
-            )
-
-    def mouse_callback(
-            self,
-            window,
-            xpos,
-            ypos
-    ):
+    def mouse_callback(self,window,xpos,ypos):
 
         if self.first_mouse:
 
@@ -86,10 +63,7 @@ class Application:
         self.lastX = xpos
         self.lastY = ypos
 
-        self.camera.process_mouse_movement(
-            xoffset,
-            yoffset
-        )
+        self.camera.process_mouse_movement(xoffset,yoffset)
 
     # ======================================================
     # OPENGL
@@ -98,43 +72,19 @@ class Application:
     def init_opengl(self):
 
         if not glfw.init():
+            raise RuntimeError("Erro GLFW")
 
-            raise RuntimeError(
-                "Erro GLFW"
-            )
+        self.window = glfw.create_window(self.width,self.height,"OpenGL Moderno",None,None)
 
-        self.window = glfw.create_window(
-            self.width,
-            self.height,
-            "OpenGL Moderno",
-            None,
-            None
-        )
+        glfw.make_context_current(self.window)
 
-        glfw.make_context_current(
-            self.window
-        )
+        glfw.set_window_size_callback(self.window,self.resize_callback)
 
-        glfw.set_window_size_callback(
-            self.window,
-            self.resize_callback
-        )
+        glfw.set_key_callback(self.window,self.key_callback)
 
-        glfw.set_key_callback(
-            self.window,
-            self.key_callback
-        )
+        glfw.set_cursor_pos_callback(self.window,self.mouse_callback)
 
-        glfw.set_cursor_pos_callback(
-            self.window,
-            self.mouse_callback
-        )
-
-        glfw.set_input_mode(
-            self.window,
-            glfw.CURSOR,
-            glfw.CURSOR_DISABLED
-        )
+        glfw.set_input_mode(self.window,glfw.CURSOR,glfw.CURSOR_DISABLED)
 
         glEnable(GL_DEPTH_TEST)
 
@@ -186,10 +136,7 @@ class Application:
         }
         """
 
-        self.shader = Shader(
-            vertex_src,
-            fragment_src
-        )
+        self.shader = Shader(vertex_src, fragment_src)
 
     # ======================================================
     # INPUT
@@ -197,45 +144,17 @@ class Application:
 
     def process_input(self, vel):
 
-        if glfw.get_key(
-                self.window,
-                glfw.KEY_W
-        ) == glfw.PRESS:
+        if glfw.get_key(self.window,glfw.KEY_W) == glfw.PRESS:
+            self.camera.process_keyboard("FORWARD",vel)
 
-            self.camera.process_keyboard(
-                "FORWARD",
-                vel
-            )
+        if glfw.get_key(self.window,glfw.KEY_S) == glfw.PRESS:
+            self.camera.process_keyboard("BACKWARD",vel)
 
-        if glfw.get_key(
-                self.window,
-                glfw.KEY_S
-        ) == glfw.PRESS:
+        if glfw.get_key(self.window,glfw.KEY_A) == glfw.PRESS:
+            self.camera.process_keyboard("LEFT",vel)
 
-            self.camera.process_keyboard(
-                "BACKWARD",
-                vel
-            )
-
-        if glfw.get_key(
-                self.window,
-                glfw.KEY_A
-        ) == glfw.PRESS:
-
-            self.camera.process_keyboard(
-                "LEFT",
-                vel
-            )
-
-        if glfw.get_key(
-                self.window,
-                glfw.KEY_D
-        ) == glfw.PRESS:
-
-            self.camera.process_keyboard(
-                "RIGHT",
-                vel
-            )
+        if glfw.get_key(self.window,glfw.KEY_D) == glfw.PRESS:
+            self.camera.process_keyboard("RIGHT",vel)
 
     # ======================================================
     # RENDER
@@ -243,46 +162,21 @@ class Application:
 
     def render(self):
 
-        glClearColor(
-            0.1,
-            0.1,
-            0.1,
-            1.0
-        )
+        glClearColor(0.1,0.1,0.1,1.0)
 
-        glClear(
-            GL_COLOR_BUFFER_BIT |
-            GL_DEPTH_BUFFER_BIT
-        )
+        glClear(GL_COLOR_BUFFER_BIT |GL_DEPTH_BUFFER_BIT)
 
         self.shader.use()
 
-        view = (
-            self.camera.get_view_matrix()
-        )
+        view = self.camera.get_view_matrix()
 
-        projection = (
-            pyrr.matrix44
-            .create_perspective_projection_matrix(
-                45.0,
-                self.width / self.height,
-                0.1,
-                100.0
-            )
-        )
+        projection = pyrr.matrix44.create_perspective_projection_matrix(45.0,self.width / self.height,0.1,100.0)
 
-        self.shader.set_matrix4(
-            "view",
-            view
-        )
+        self.shader.set_matrix4("view",view)
 
-        self.shader.set_matrix4(
-            "projection",
-            projection
-        )
+        self.shader.set_matrix4("projection",projection)
 
         for model in self.models:
-
             model.draw(self.shader)
 
     # ======================================================
@@ -301,9 +195,7 @@ class Application:
 
         base_speed = 10.0
 
-        while not glfw.window_should_close(
-                self.window
-        ):
+        while not glfw.window_should_close(self.window):
 
             current = glfw.get_time()
 
@@ -317,9 +209,7 @@ class Application:
 
             self.render()
 
-            glfw.swap_buffers(
-                self.window
-            )
+            glfw.swap_buffers(self.window)
 
             glfw.poll_events()
 
